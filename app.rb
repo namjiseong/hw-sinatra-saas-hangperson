34a689760737ffe4a181c0a97387431900baaf98
namjiseong <skawltjd2000@naver.com>
2020-12-13 07:12:16 +0000

    hw

--- app.rb
+++ app.rb
@@ -29,8 +29,8 @@ class HangpersonApp < Sinatra::Base
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || HangpersonGame.get_random_word
     # NOTE: don't change previous line - it's needed by autograder!
-
-    @game = HangpersonGame.new(word)
+    
+    @game = HangpersonGame.new("abcde")
     redirect '/show'
   end
   
@@ -40,6 +40,18 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      valid = @game.guess(letter)
+    rescue ArgumentError => e
+       flash[:message]="Invalid guess."
+       redirect '/show'
+    end
+    if not valid
+      flash[:message]="You have already used that letter."
+    else
+      @game.guess(letter)
+    end
+    ##
     redirect '/show'
   end
   
@@ -49,17 +61,27 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
+    
     ### YOUR CODE HERE ###
+    
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    end
+    ##
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/show' if @game.check_win_or_lose != :win
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/show' if @game.check_win_or_lose != :lose
     erb :lose # You may change/remove this line
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,11 +7,59 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
-
+  
+  def guess(crt)
+    a = ('a'..'z')
+    b = ('A'..'Z')
+    if crt.nil? or crt.empty?
+      raise ArgumentError
+    end
+    unless a.include? crt or b.include? crt
+      raise ArgumentError
+    end
+    
+    crt.downcase!
+    
+    if guesses.include? crt or wrong_guesses.include? crt
+      return false
+    elsif @word.include? crt
+      @guesses << crt
+    else
+      @wrong_guesses << crt
+    end
+    return true
+  end
+  
+  def word_with_guesses
+    str = ''
+    @word.each_char do |x|
+      if @guesses.include? x
+        str << x
+      else
+        str << '-'
+      end
+    end
+    str
+  end
+  
+  def check_win_or_lose
+    unless word_with_guesses.include? '-'
+      return :win
+    end
+    if wrong_guesses.length >= 7
+      return :lose
+    end
+    return :play
+  end
+  
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
@@ -23,5 +71,5 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
-
+#wo4snakes
 end
